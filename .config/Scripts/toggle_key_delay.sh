81c709cf466ea47daa3854f8cea90a83f2b21f88
renzhamin <tasnimferdous9@iut-dhaka.edu>
2022-10-22 00:07:42 +0600

    changed submodule paths to https and updated keyboard repeat delay script

---
+++ .config/Scripts/toggle_key_delay.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+repeat_rate=$(xset q | grep  "repeat rate" | sed -E 's/.*repeat rate:\s*([0-9]+)/\1/')
+
+if [ "${repeat_rate}" -lt 30 ]; then
+    xset r rate 210  30
+else
+    xset r rate 660 25
+fi
